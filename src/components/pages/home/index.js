af215f7ed6c99d5b044cd528b39cd4eef30046f2
Steijn Ploegmakers <461991@student.fontys.nl>
2022-09-05 12:58:07 +0200

    initial commit

---
+++ src/components/pages/home/index.js
@@ -0,0 +1,6 @@
+function Index() {
+    return <div>
+        <h1>FreelanceDJ</h1>
+    </div>
+}
+export default Index
---
+++ src/router.js
@@ -0,0 +1,11 @@
+import {Route} from "react-router-dom";
+import Index from "./components/pages/home";
+
+
+function Router() {
+    return <div>
+        <Route path="/index" component={Index}/>
+    </div>
+}
+
+export default Router
