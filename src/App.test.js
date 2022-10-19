fe5d9434558bc76653e8479e983ff1fdb6ee7496
Steijn Ploegmakers <461991@student.fontys.nl>
2022-10-19 15:36:55 +0200

    feature: added test to applicatiom

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import React from "react";
 
+test('render app', () => {
+  render(
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>);
+});
