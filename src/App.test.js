83a25a66c98961f50ef9c31b2cd1ee83fb8765a3
Steijn Ploegmakers <461991@student.fontys.nl>
2022-12-14 18:34:53 +0100

    made 3 tests for the DJCard component

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,38 @@
+import {screen, render} from "@testing-library/react";
+import DJCard from "./components/pages/dj/djcard";
+import {rest} from "msw";
+import {setupServer} from "msw/node";
 import React from "react";
 
+const djNameResponse = rest.get("https://localhost:7284/api/djaccount", (req, res, ctx) => {
+    return res(ctx.json([{id: 1, name: "Hardwell", description: "Zieke Techno DJ", email: "dj@hardwellmgmt.com", phone: 316759434, price: 4560}]));
+});
+
+const handlers = [djNameResponse];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test("It should have the correct Dj called Hardwell",async() => {
+    render(<DJCard/>)
+    const djName = await screen.findByText("Hardwell");
+    expect(djName).toBeVisible();
+
+})
+
+test("It should have the correct Dj called Hardwell with the description Zieke Techno DJ",async() => {
+    render(<DJCard/>)
+    const djName = await screen.findByText("Zieke Techno DJ")
+    expect(djName).toBeVisible();
+
+})
+
+test("It should have the correct Dj called Hardwell with the price 4560",async() => {
+    render(<DJCard/>)
+    const djName = await screen.findByText(4560)
+    expect(djName).toBeVisible();
+
+})
