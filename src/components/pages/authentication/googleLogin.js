890f791461240761ffd9efa8e1cf5dcfeb9074f6
Steijn Ploegmakers <461991@student.fontys.nl>
2022-12-20 17:39:47 +0100

    changed some useStates

--- src/components/pages/authentication/googleLogin.js
+++ src/components/pages/authentication/googleLogin.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {GoogleLogin} from "@react-oauth/google";
 import jwtDecode from "jwt-decode";
 import {useHistory} from "react-router-dom";
 
+
+
 function Login() {
 
     const history = useHistory()
+    const [user, setUser] = useState(null)
 
     const onSuccess = (res) => {
         console.log("Login success!")
         const userObject = jwtDecode(res.credential)
         console.log(userObject)
 
-        const formData = new FormData();
-        formData.append("name", userObject.name)
-        formData.append("email", userObject.email)
 
         const requestOptions = {
             method: 'POST',
-            headers: {'Content-Type': 'multipart/form-data'},
-            body: formData
+            headers: {'Content-Type': 'application/json'},
+            //body: ;
         }
 
         fetch("https://localhost:7023/api/user",
-            requestOptions).then(response => response.json())
-        history.push("/djs")
+            requestOptions).then(async (response) => {
+                const data = await response.json()
+                setUser(data)
+            history.push("/djs")
+            })
     }
     const onFailure = (res) => {
         console.log("Login failure! res:", res)
@@ -36,7 +39,6 @@ function Login() {
             onSuccess={onSuccess}
             onFailure={onFailure}
         />
-
     </div>;
 }
 
